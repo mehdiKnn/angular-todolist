e7c461e068cdcbdb248b02446e3e3919f495b59a
Amelie NGUYEN <amelienguyen@2400607.local>
2022-11-15 11:09:53 +0100

    add task, css base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+  
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todolist';
+
+    public items: string[] = [];
+  
+    public newTask = '';
+  
+    public addToList() {
+        if (this.newTask == '') {
+        }
+        else {
+            this.items.push(this.newTask);
+            this.newTask = '';
+        }
+    }
+  
+    public deleteTask(index: number) {
+        this.items.splice(index, 1);
+    }
 }
