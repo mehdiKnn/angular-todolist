ce1b6a0d51d78b4d87aaf825b3e2577df4dd54c0
Antoine Carbonnel <antoine@kannounnel.dev>
2022-11-17 11:33:03 +0100

    feat: change task to Object && add completed feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,47 @@
-import { Component } from '@angular/core';
-  
+import {Component} from '@angular/core';
+import {Task} from "./models/task";
+import {iif} from "rxjs";
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
-    public tasks: string[] = [];
-  
-    public newTask = '';
-  
-    public addToList() {
-        if (this.newTask == '') {
-        }
-        else {
-            this.tasks.push(this.newTask);
-            this.newTask = '';
-        }
+
+  public tasks: Task[] = [];
+
+  public newTask: Task = {
+    id: 0,
+    value: '',
+    done: false
+  };
+
+  public addToList() {
+    if (this.newTask.value !== '') {
+      this.tasks.push({id: this.tasks.length, value: this.newTask.value, done: false});
+      this.newTask.value = '';
     }
-  
-    public deleteTask(index: number) {
-        this.tasks.splice(index, 1);
+  }
+
+  public completeTask(id: number) {
+    const taskFind = this.tasks.find(task => task.id === id);
+    if (taskFind) {
+      taskFind.done = true;
     }
+  }
+
+  public tasksTodo() {
+    return this.tasks.filter(task => !task.done)
+  }
+
+  public tasksDone() {
+    return this.tasks.filter(task => task.done)
+  }
+
+  public completedTasksLength() {
+    return this.tasksDone().length;
+  }
 }
